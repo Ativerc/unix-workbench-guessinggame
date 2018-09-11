2ba2858df3dd17a82df4f2891f5808bd97db9d8f
Pratyush Mohapatra <ppmvcs@gmail.com>
2018-09-11 08:41:16 +0530

    Check program after every feature addition.

--- guessinggame.sh
+++ guessinggame.sh
@@ -7,19 +7,20 @@ echo "Guess the number of files in the current directory:"
 read user_input
 
 function checker {
-while [ $user_input -ne $number_of_files ]
-do
-if [ $user_input -gt $number_of_files ]
-then
-    echo "Higher than the actual value! Please try again!"
-    read user_input
-else
-    echo "Lower than the actual value! Please try again!"
-    read user_input
-fi
-done
-
+    while [ $user_input -ne $number_of_files ]
+    do
+    if [ $user_input -gt $number_of_files ]
+    then
+        echo "Higher than the actual value! Please try again!"
+        read user_input
+    else
+        echo "Lower than the actual value! Please try again!"
+        read user_input
+    fi
+    done
 }
+
+checker
 
 echo "Congratulations! You guessed the right number of files: $number_of_files !"
 
